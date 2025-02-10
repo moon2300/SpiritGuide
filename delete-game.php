8f6840f8a112db68bd256a3bd4267cb5f598f77b
moon2300 <alhassan965@gmail.com>
2025-02-10 14:59:50 +0100

    delete config file

--- delete-game.php
+++ delete-game.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/api.php';
 
-$url ='https://highscores.martindilling.com/api/v1/games';
+$url ='https://highscores.martindilling.com/api/v1/games/';
 $response = apiDelete($url);
 
 echo '<pre>';
